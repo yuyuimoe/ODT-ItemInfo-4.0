770d0cf7c6c36755fdf9f1e4f8bae14b084919b9
Kobe Thuy <thuynguyentrungdang@gmail.com>
2026-02-25 01:05:04 +0700

    Refactored `BsgBlackList`

---
+++ Models/ModBsgBlackList.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ItemInfo.Models;
+
+public class ModBsgBlackList
+{
+    [JsonPropertyName("bsgBlackList")]
+    public List<string> BsgBlackList { get; set; } = [];
+}
